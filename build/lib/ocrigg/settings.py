48b34037ee88b9089dd8aae1f3f935f5a9f073e8
Martin Skow Røed <msroed@gmail.com>
2014-01-15 16:51:15 +0100

    first commit

---
+++ build/lib/ocrigg/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for ocrigg project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'ocrigg'
+
+SPIDER_MODULES = ['ocrigg.spiders']
+NEWSPIDER_MODULE = 'ocrigg.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'ocrigg (+http://www.yourdomain.com)'
---
+++ build/lib/ocrigg/spiders/ocrigg_spider.py
@@ -0,0 +1,42 @@
+from scrapy.spider import BaseSpider
+from scrapy.http import Request
+from scrapy.selector import HtmlXPathSelector
+from ocrigg.items import OcriggItem
+import re
+
+class OcSpider(BaseSpider):
+	name = 'ocrigg'
+	allowed_domains = ['pipelineme.com']
+	start_urls = ['http://www.pipelineme.com/research/land-rigs/']
+
+	def parse(self, response):
+		hxs = HtmlXPathSelector(response)
+		countries = [x for x in hxs.select('//select[@name="Country"]/option/@value').extract() if x]
+		for country in countries:
+			url = "http://www.pipelineme.com/research/land-rigs?Country=%s" % (country)
+			yield Request(url=url, callback=self.yield_all_pages_from_country)
+
+	def yield_all_pages_from_country(self, response):
+		hxs = HtmlXPathSelector(response)
+		last = int(hxs.select('//div[@class="pagination"]//a/@href').extract()[-2][-1])
+		for page in range(1, last+1):
+			url = "%s&page=%s" % (response.url, page)
+			yield Request(url=url, callback=self.yield_riggs)
+
+	def yield_riggs(self, response):
+		hxs = HtmlXPathSelector(response)
+		rigger = hxs.select('//ul[@class="news_pad"]//a/@href').extract()
+		for rigg in rigger:
+			url = "http://www.pipelineme.com%s" % (rigg)
+			yield Request(url=url, callback=self.parse_rigg)
+
+	def parse_rigg(self, response):
+		hxs = HtmlXPathSelector(response)
+		for elem in hxs.select('//p[@class="details_01"]'):
+			item = OcriggItem()
+			item['info'] = re.sub(r'\W+', '', elem.select('text()').extract()[0])
+			item['description'] = elem.select('span/text()').extract()[0].strip()
+			yield item
+		
+
+
